bf14b101f0f11e762e893d50ca547d535c702de4
epeddireddy <epeddireddy@thetus.com>
2014-07-30 20:01:14 -0700

    Initial setup with simple view using bootstrap working.

---
+++ server.js
@@ -0,0 +1,61 @@
+// Modules
+var fs = require('fs'),
+	express = require('express'),
+	path = require('path'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	consolidate = require('consolidate'),
+	Handlebars = require('handlebars');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', consolidate.handlebars);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/server-templates');
+
+var partials = "./server-templates/partials/";
+fs.readdirSync(partials).forEach(function (file) {
+  var source = fs.readFileSync(partials + file, "utf8"),
+      partial = /(.+)\.html/.exec(file).pop();
+  Handlebars.registerPartial(partial, source);
+});
+
+// express routes
+
+app.get('/', function (req, res) {
+  res.render('./index.html');
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 4444);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port # ' + app.get('port'));
+});
