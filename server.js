6dc47f2ff67b8b8b575513a48a41cd4820c76fa0
Geoffrey Unger <geoffunger@gmail.com>
2014-08-03 21:25:55 -0700

    Added orchestrate requirement for db access - do 'npm install' to install. Added config.js to gitignore - config.js should be created locally and contain module.exports = {dbKey:"the-orchestrate-key"}

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Modules
 var fs = require('fs'),
-	express = require('express'),
-	path = require('path'),
-	logger = require('morgan'),
-	bodyParser = require('body-parser'),
-	consolidate = require('consolidate'),
-	Handlebars = require('handlebars');
+    express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    consolidate = require('consolidate'),
+    Handlebars = require('handlebars');
+
+var db = require('orchestrate')(config.dbKey);
 
 var app = express();
 
@@ -19,25 +21,25 @@ app.set('views', __dirname + '/server-templates');
 
 var partials = "./server-templates/partials/";
 fs.readdirSync(partials).forEach(function (file) {
-  var source = fs.readFileSync(partials + file, "utf8"),
-      partial = /(.+)\.html/.exec(file).pop();
-  Handlebars.registerPartial(partial, source);
+    var source = fs.readFileSync(partials + file, "utf8"),
+        partial = /(.+)\.html/.exec(file).pop();
+    Handlebars.registerPartial(partial, source);
 });
 
 // express routes
 
 app.get('/', function (req, res) {
-  res.render('./index.html');
+    res.render('./index.html');
 });
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -46,7 +48,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -56,6 +58,6 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 4444);
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port # ' + app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port # ' + app.get('port'));
 });
