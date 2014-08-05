2b0527a88873a0e536387ee1ef43c11d4e17b89c
Geoffrey Unger <geoffunger@gmail.com>
2014-08-04 21:00:23 -0700

    Snapshot of work on beers list

--- public/js/views/find-beer-view.js
+++ public/js/views/find-beer-view.js
@@ -1,21 +1,26 @@
 var $ = require('jquery'),
-	Backbone = require('backbone');
-	
+    Backbone = require('backbone');
+
 Backbone.$ = $;
+
+var Beers = require('../collections/beers');
 
 // Bring in template for main view
 var findBeerTemplate = require('../../templates/find-beer.hbs');
 
 var FindBeerTemplate = Backbone.View.extend({
-	el: '#my-app',
-	initialize: function () {
-		console.log('Yay find beer view!');
-		$(this.el).html(findBeerTemplate);
-	},
-	render: function () {
-		// do something
-		console.log('Yay find beer view render');
-	}
+    el: '#my-app',
+    collection: new Beers(),
+    initialize: function () {
+        window.beerCollection = this.collection;
+        this.collection.fetch();
+        console.log('Yay find beer view!');
+        $(this.el).html(findBeerTemplate);
+    },
+    render: function () {
+        // do something
+        console.log('Yay find beer view render');
+    }
 });
 
 module.exports = FindBeerTemplate;
