3648f633947b026a965f24e9bca57ad980a721fc
Geoff Unger <geoff@shopigniter.com>
2014-08-13 17:05:57 -0700

    Added bare View and template files for ratings-add and ratings-view

---
+++ public/js/views/rating-add-view.js
@@ -0,0 +1,13 @@
+var $ = require('jQuery'), Backbone = require('backbone');
+
+Backbone.$ = $;
+
+var ratingAddTemplate = require('../../templates/rating-add-view.hbs');
+
+var RatingAddView = Backbone.View.extend(
+    {
+
+    }
+)
+
+module.exports = RatingAddView;
---
+++ public/js/views/rating-display-view.js
@@ -0,0 +1,13 @@
+var $ = require('jQuery'), Backbone = require('backbone');
+
+Backbone.$ = $;
+
+var ratingDisplayTemplate = require('../../templates/rating-display-view.hbs');
+
+var RatingDisplayView = Backbone.View.extend(
+    {
+
+    }
+)
+
+module.exports = RatingDisplayView;
