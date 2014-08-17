1e78ac3da0477a1459aa26ec972f67d4b0e3cda6
Geoffrey Unger <geoffunger@gmail.com>
2014-08-17 15:35:58 -0700

    Rating now persisting to DB as property avgRating on the beer object

--- public/js/views/drinking-now-input-view.js
+++ public/js/views/drinking-now-input-view.js
@@ -5,30 +5,35 @@ Backbone.$ = $;
 
 var DrinkingInputView = Backbone.View.extend({
 
-  el: '.form-group',
-  events: {
-    'click #add-beer': 'addBeer'
-  },
-  addBeer: function () {
-    var $beerName = $(this.el).find('#beer-input');
-    var $barName = $(this.el).find('#bar-input');
+    el: '.form-group',
+    events: {
+        'click #add-beer': 'addBeer'
+    },
+    addBeer: function () {
+        var $beerName = $(this.el).find('#beer-input');
+        var $barName = $(this.el).find('#bar-input');
+        var $beerRating = $(this.el).find('#beer-rating');
 
-    var beerName = $beerName.val();
-    var barName = $barName.val();
-    var collectionFromInput = {
-      beer: {
-        name: beerName,
-        creationDate: Date.now(),
-        bars: [{
-          barName: barName
-        }]
-      }
-    };
+        var beerName = $beerName.val();
+        var barName = $barName.val();
+        var beerRating = $beerRating.val();
+        var collectionFromInput = {
+            beer: {
+                name: beerName,
+                creationDate: Date.now(),
+                bars: [
+                    {
+                        barName: barName
+                    }
+                ],
+                avgRating: beerRating
+            }
+        };
 
-    this.collection.create( collectionFromInput, {validate: true} );
-    $beerName.val('');
-    $barName.val('');
-  }
+        this.collection.create(collectionFromInput, {validate: true});
+        $beerName.val('');
+        $barName.val('');
+    }
 });
 
 module.exports = DrinkingInputView;
