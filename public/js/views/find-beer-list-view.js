2ddbed2c48f9a9eb29a34af8361f2284c7aada30
Geoffrey Unger <geoffunger@gmail.com>
2014-08-20 20:44:27 -0700

    Updated date now working on Find Beer view TODO - update drinking now views to store last updated in "updated" property

--- public/js/views/find-beer-list-view.js
+++ public/js/views/find-beer-list-view.js
@@ -16,13 +16,45 @@ var BarsListView = Backbone.View.extend({
     },
     render: function (beerName) {
         console.log("Yay BarsListView render");
+        function timeSince(date) {
+
+            var seconds = Math.floor((new Date() - date) / 1000);
+
+            var interval = Math.floor(seconds / 31536000);
+
+            if (interval > 1) {
+                return interval + " years";
+            }
+            interval = Math.floor(seconds / 2592000);
+            if (interval > 1) {
+                return interval + " months";
+            }
+            interval = Math.floor(seconds / 86400);
+            if (interval > 1) {
+                return interval + " days";
+            }
+            interval = Math.floor(seconds / 3600);
+            if (interval > 1) {
+                return interval + " hours";
+            }
+            interval = Math.floor(seconds / 60);
+            if (interval > 1) {
+                return interval + " minutes";
+            }
+            return Math.floor(seconds) + " seconds";
+        }
         var found = this.collection.filter(function(item){
             return(beerName == item.get('beer').name);
         });
 
-        var data = found[0].attributes.beer.bars;
-        console.log("data is: " + data);
-        this.$el.html(barsListTemplate({barData: data}));
+        var barData = found[0].attributes.beer.bars;
+        barData.forEach(function(bar, index, bars){
+            var timeSinceString = timeSince(bar.updated);
+            bars[index].timeSinceString = timeSinceString;
+
+        });
+        console.log("data is: " + barData);
+        this.$el.html(barsListTemplate({barData: barData}));
 
         //Show the rating for the beer
         var ratingDisplayView = new RatingDisplayView({collection:this.collection});
