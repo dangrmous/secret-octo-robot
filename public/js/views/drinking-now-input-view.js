1dca47020c4687e868c827823140dd0b20c6e7a1
epeddireddy <epeddireddy@thetus.com>
2014-08-26 19:18:16 -0700

    Added dropdown and custom input to drinking now view. Cleaned up some code.

--- public/js/views/drinking-now-input-view.js
+++ public/js/views/drinking-now-input-view.js
@@ -1,5 +1,8 @@
 var $ = require('jquery'),
-    Backbone = require('backbone');
+    Backbone = require('backbone'),
+    Beers = require('../collections/beers'),
+    drinkingNowTemplate = require('../../templates/drinking-now.hbs'),
+    newBeer = false;
 
 Backbone.$ = $;
 
@@ -8,34 +11,67 @@ var DrinkingInputView = Backbone.View.extend({
     el: '.form-group',
     events: {
         'click #add-beer': 'addBeer',
+        'click #add-beer-btn': 'addNewBeer',
+        'click #go-back': 'goBackToList',
         'change :radio':
             function(){
                 $('.choice').text( $('input:checked').val() + ' stars' );
             }
+    },
+    collection: new Beers(),
+    initialize: function () {
+        this.listenTo(this.collection,'all', this.render);
+    },
+    render: function () {
 
+        var newBeerInput = $('#add-new-beer'),
+            goBackBtn = $('#go-back'),
+            data = [];
+
+        newBeerInput.hide();
+        goBackBtn.hide();
+
+        // Add beer and bar to list
+        this.collection.models.forEach(function (item) {
+            console.log('ITEM' + item.attributes.beer);
+            data.push({
+                name: item.attributes.beer.name
+            });
+        });
+
+        // Update table
+        if (!newBeer) {
+            this.$('#beer-input').html(drinkingNowTemplate({ beerData: data }));
+        } else {
+            newBeerInput.html(drinkingNowTemplate({ beerData: data }));
+        }
     },
     addBeer: function () {
-        var $beerName = $(this.el).find('#beer-input');
-        var $barName = $(this.el).find('#bar-input');
-        var $beerRating = $(this.el).find('input:checked');
+        var $beerName;
 
-        var beerName = $beerName.val();
-        var barName = $barName.val();
-        var beerRating = $beerRating.val();
-        var collectionFromInput = {
-            beer: {
-                name: beerName,
-                creationDate: Date.now(),
-                bars: [
-                    {
-                        barName: barName,
-                        updated: Date.now()
-                    }
-                ],
-                avgRating: beerRating,
-                numEntries: (beerRating ? 1 : 0)
-
-            }
+        if (!newBeer) {
+            $beerName = $(this.el).find('#beer-input');
+        } else {
+            $beerName = $(this.el).find('#add-new-beer');
+        }
+        var $barName = $(this.el).find('#bar-input'),
+            $beerRating = $(this.el).find('input:checked'),
+            beerName = $beerName.val(),
+            barName = $barName.val(),
+            beerRating = $beerRating.val(),
+            collectionFromInput = {
+                beer: {
+                    name: beerName,
+                    creationDate: Date.now(),
+                    bars: [
+                        {
+                            barName: barName,
+                            updated: Date.now()
+                        }
+                    ],
+                    avgRating: beerRating,
+                    numEntries: (beerRating ? 1 : 0)
+                }
         };
 
         this.collection.create(collectionFromInput, {validate: true});
@@ -45,6 +81,38 @@ var DrinkingInputView = Backbone.View.extend({
             $(this).removeAttr('checked');
         });
         $('h4.choice').text('Rate this beer');
+
+        if (newBeer) {
+            $('#add-new-beer').hide();
+            $('#beer-input').show();
+            newBeer = false;
+        }
+    },
+    addNewBeer: function () {
+        var beerList = $('#beer-input'),
+            newBeerInput = $('#add-new-beer'),
+            goBackBtn = $('#go-back'),
+            addBeerBtn = $('#add-beer-btn');
+
+        newBeer = true;
+
+        addBeerBtn.hide();
+        beerList.hide();
+        newBeerInput.show();
+        goBackBtn.show();
+    },
+    goBackToList: function () {
+        var beerList = $('#beer-input'),
+            newBeerInput = $('#add-new-beer'),
+            goBackBtn = $('#go-back'),
+            addBeerBtn = $('#add-beer-btn');
+
+        newBeer = false;
+
+        addBeerBtn.show();
+        beerList.show();
+        newBeerInput.hide();
+        goBackBtn.hide();
     }
 });
 
